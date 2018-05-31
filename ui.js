c4fedd1f90e3169bffddf67bcf74747ea363333d
Aga <agn.sierocinska@gmail.com>
2018-05-31 22:02:43 +0200

    show user repositories UI

--- ui.js
+++ ui.js
@@ -9,7 +9,7 @@ class UI {
             <div class="row">
                 <div class="col-md-3">
                 <img class="img-fluid mb-2" src="${user.avatar_url}">
-                <a href="${user.htm_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
+                <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
                 </div>
                 <div class="col-md-9">
                 <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
@@ -30,6 +30,29 @@ class UI {
         <h3 class="page-heading mb-3">Latest Repos</h3>
         <div id="repos"></div>
         `;
+    }
+
+    //Show user repos
+    showRepos(repos) {
+        let output = '';
+        repos.forEach(function (repo) {
+            output += `
+            <div class="card card-body mb-2">
+                <div class="row">
+                    <div class="col-md-6">
+                        <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                    </div>
+                    <div class="col-md-6">
+                     <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+                     <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+                     <span class="badge badge-success">Forks: ${repo.forks_count}</span>
+                    </div>
+                </div>
+            </div>
+            `;
+        });
+        //Output repositories
+        document.getElementById('repos').innerHTML = output;
     }
 
     showAlert(message, className) {
@@ -55,7 +78,7 @@ class UI {
 
     //Clear alert message
     clearAlert() {
-        const currentAlert = document.querySelector('.alert')
+        const currentAlert = document.querySelector('.alert');
         if (currentAlert) {
             currentAlert.remove()
         }
